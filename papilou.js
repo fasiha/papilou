a2dded225585f5a9bce9ab175015399ec3f27b54
Ahmed Fasih <wuzzyview@gmail.com>
2025-09-13 16:10:05 -0700

    html

--- papilou.js
+++ papilou.js
@@ -1,7 +1,7 @@
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 
-import { result } from "./emoji-test.js";
+import { result as CATEGORY_SUBCAT_EMOJI } from "./emoji-test.js";
 import { parse as parseJapanese } from "./jmdict.js";
 import { originalToDiacritics } from "./arabic.js";
 import { originalToPinyin } from "./chinese.js";
@@ -72,10 +72,10 @@ function isEmptyObj(obj) {
 
 function makeMarkdown() {
   const lines = [];
-  for (const category in result) {
+  for (const category in CATEGORY_SUBCAT_EMOJI) {
     lines.push(`## ${category}`);
-    for (const subcategory in result[category]) {
-      const table = emojisToTable(result[category][subcategory]);
+    for (const subcategory in CATEGORY_SUBCAT_EMOJI[category]) {
+      const table = emojisToTable(CATEGORY_SUBCAT_EMOJI[category][subcategory]);
       if (isEmptyObj(table)) continue;
       lines.push(`### ${category} / ${subcategory}`);
       lines.push(objToString(table));
@@ -85,4 +85,45 @@ function makeMarkdown() {
   return lines.join("\n");
 }
 
+function makeHtml() {
+  const lines = [];
+  lines.push(`<html><head><meta charset="utf-8"><title>Papilou</title>`);
+  lines.push('<link rel="stylesheet" href="./papilou.css"></style>');
+  lines.push(`</head><body>`);
+  lines.push(`<table>`);
+
+  const makeHeaderRow = (cells) =>
+    `<tr class="languages">${cells.map((c) => `<th>${c}</th>`).join("")}</tr>`;
+  const makeSingleRow = (content) =>
+    `<tr class="category"><th colspan="${
+      languages.length + 1
+    }">${content}</th></tr>`;
+
+  lines.push("<thead>");
+  lines.push(makeHeaderRow(["Emoji", ...languages]));
+  lines.push("</thead>");
+  for (const category in CATEGORY_SUBCAT_EMOJI) {
+    for (const subcategory in CATEGORY_SUBCAT_EMOJI[category]) {
+      const table = emojisToTable(CATEGORY_SUBCAT_EMOJI[category][subcategory]);
+      if (isEmptyObj(table)) continue;
+      lines.push(makeSingleRow(`${category} / ${subcategory}`));
+
+      for (const emoji in table) {
+        const cells = [
+          `<td>${emoji}</td>`,
+          ...languages.map(
+            (lang) => `<td lang="${lang}">${table[emoji][lang] || ""}</td>`
+          ),
+        ];
+        const row = cells.join("");
+        lines.push(`<tr>${row}</tr>`);
+      }
+    }
+  }
+  lines.push(`</table>`);
+  lines.push(`</body></html>`);
+  return lines.join("\n");
+}
+
 writeFileSync("papilou.md", makeMarkdown(), "utf8");
+writeFileSync("index.html", makeHtml(), "utf8");
